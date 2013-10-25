33965591b109c5bb6e54b3264173f2103343003c
Luke Champine <luke.champine@gmail.com>
2013-10-24 22:50:40 -0400

    move some definitions around

--- drawing.go
+++ drawing.go
@@ -26,8 +26,35 @@ func (v Vector) Unit() Vector {
 	return Vector{v.X / l, v.Y / l, v.Z / l}
 }
 
+type Matrix [3][3]float64
+
+func (m Matrix) Multiply(v Vector) Vector {
+	return Vector{
+		v.X*m[0][0] + v.Y*m[0][1] + v.Z*m[0][2],
+		v.X*m[1][0] + v.Y*m[1][1] + v.Z*m[1][2],
+		v.X*m[2][0] + v.Y*m[2][1] + v.Z*m[2][2]}
+}
+
+func RotationMatrix(angle float64, v Vector) Matrix {
+	s := math.Sin(angle * math.Pi / 180)
+	c := math.Cos(angle * math.Pi / 180)
+	v = v.Unit()
+	return [3][3]float64{
+		[3]float64{v.X*v.X*(1-c) + c, v.X*v.Y*(1-c) - v.Z*s, v.X*v.Y*(1-c) + v.Y*s},
+		[3]float64{v.X*v.Y*(1-c) + v.Z*s, v.Y*v.Y*(1-c) + c, v.Y*v.Z*(1-c) - v.X*s},
+		[3]float64{v.X*v.Z*(1-c) - v.Y*s, v.Y*v.Z*(1-c) + v.X*s, v.Z*v.Z*(1-c) + c}}
+}
+
 type Line struct {
 	V1, V2 Vector
+}
+
+func (l Line) Transform(m Matrix) Line {
+	return Line{m.Multiply(l.V1), m.Multiply(l.V2)}
+}
+
+func (l Line) Projection() Line {
+	return Line{Vector{l.V1.X, l.V1.Y, 0}, Vector{l.V2.X, l.V2.Y, 0}}
 }
 
 type Canvas struct {
@@ -47,6 +74,10 @@ func NewCanvas(width, height int) *Canvas {
 	return canvas
 }
 
+func (c Canvas) SmartSet(x, y int, color color.RGBA) {
+	c.Set(x+c.Bounds().Size().X/2, -y+c.Bounds().Size().Y/2, color)
+}
+
 func (c Canvas) DrawLine(l Line) {
 	length := l.V2.Sub(l.V1).Length()
 	unit := l.V2.Sub(l.V1).Unit()
@@ -54,7 +85,7 @@ func (c Canvas) DrawLine(l Line) {
 		x := l.V1.X + float64(i)*unit.X
 		y := l.V1.Y + float64(i)*unit.Y
 		// adjust for canvas coordinate system
-		c.Set(int(x)+c.Bounds().Size().X/2, int(-y)+c.Bounds().Size().Y/2, color.RGBA{0, 0, 0, 255})
+		c.SmartSet(int(x), int(y), color.RGBA{0, 0, 0, 255})
 	}
 }
 
--- rotation.go
+++ rotation.go
@@ -2,39 +2,9 @@ package main
 
 import (
 	"log"
-	"math"
 	"os/exec"
 	"runtime"
 )
-
-type Matrix [3][3]float64
-
-func (m Matrix) Multiply(v Vector) Vector {
-	return Vector{
-		v.X*m[0][0] + v.Y*m[0][1] + v.Z*m[0][2],
-		v.X*m[1][0] + v.Y*m[1][1] + v.Z*m[1][2],
-		v.X*m[2][0] + v.Y*m[2][1] + v.Z*m[2][2]}
-}
-
-func (l Line) Transform(m Matrix) Line {
-	return Line{m.Multiply(l.V1), m.Multiply(l.V2)}
-}
-
-// project a 3D vector onto 2D space so we can draw it
-func (l Line) Projection() Line {
-	return Line{Vector{l.V1.X, l.V1.Y, 0}, Vector{l.V2.X, l.V2.Y, 0}}
-}
-
-// generate a transformation matrix that rotates along vector v by angle degrees
-func RotationMatrix(angle float64, v Vector) Matrix {
-	s := math.Sin(angle * math.Pi / 180)
-	c := math.Cos(angle * math.Pi / 180)
-	v = v.Unit()
-	return [3][3]float64{
-		[3]float64{v.X*v.X*(1-c) + c, v.X*v.Y*(1-c) - v.Z*s, v.X*v.Y*(1-c) + v.Y*s},
-		[3]float64{v.X*v.Y*(1-c) + v.Z*s, v.Y*v.Y*(1-c) + c, v.Y*v.Z*(1-c) - v.X*s},
-		[3]float64{v.X*v.Z*(1-c) - v.Y*s, v.Y*v.Z*(1-c) + v.X*s, v.Z*v.Z*(1-c) + c}}
-}
 
 func main() {
 	// use all available logical processors
@@ -42,7 +12,7 @@ func main() {
 	// canvas properties
 	width, height := 700, 700
 	// figure
-	lines := [12]Line{
+	figure := [12]Line{
 		Line{Vector{-100, 100, 100}, Vector{100, 100, 100}},
 		Line{Vector{100, 100, 100}, Vector{100, -100, 100}},
 		Line{Vector{100, -100, 100}, Vector{-100, -100, 100}},
@@ -65,8 +35,8 @@ func main() {
 			num := <-frameChan
 			canvas := NewCanvas(width, height)
 			tMatrix := RotationMatrix(float64(num*360/numSteps), Vector{1, 1, 1})
-			for j := range lines {
-				canvas.DrawLine(lines[j].Transform(tMatrix).Projection())
+			for j := range figure {
+				canvas.DrawLine(figure[j].Transform(tMatrix).Projection())
 			}
 			canvas.SaveToFile(num)
 		}()
