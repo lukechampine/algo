844a737d41427ff1421a3b3fafecafa467b0edd2
Luke Champine <luke.champine@gmail.com>
2013-11-02 18:31:01 -0400

    refactor triangle filling algorithm and implement backface culling

--- polyfill.go
+++ polyfill.go
@@ -7,75 +7,94 @@ import (
 	"runtime"
 )
 
-type Polygon [3]Vector
+type Triangle [3]Vector
 
-func (p Polygon) Transform(m Matrix) Polygon {
-	return Polygon{m.Multiply(p[0]), m.Multiply(p[1]), m.Multiply(p[2])}
+func (p Triangle) Transform(m Matrix) Triangle {
+	return Triangle{m.Multiply(&p[0]), m.Multiply(&p[1]), m.Multiply(&p[2])}
 }
 
-func (p Polygon) Projection() Polygon {
-	return Polygon{Vector{p[0].X, p[0].Y, 0}, Vector{p[1].X, p[1].Y, 0}, Vector{p[2].X, p[2].Y, 0}}
+func (p Triangle) IsVisible() bool {
+	// a trigon is visible if its cross product points towards the viewer
+	// since we are only concerned with the Z coordinate, we can optimize a bit
+	ux, uy := p[1].X-p[0].X, p[1].Y-p[0].Y
+	vx, vy := p[2].X-p[0].X, p[2].Y-p[0].Y
+	return (uy*vx - ux*vy) > 0
 }
 
 // algorithm taken from www-users.mat.uni.torun.pl/~wrona/3d_tutor/tri_fillers.html
-func (c Canvas) DrawPoly(poly Polygon, color color.RGBA) {
-	// sort points by y value
-	if poly[1].Y < poly[0].Y {
-		poly[1], poly[0] = poly[0], poly[1]
+func (c Canvas) DrawTri(tri Triangle, color color.RGBA) {
+	// sort points by Y value
+	if tri[1].Y < tri[0].Y {
+		tri[1], tri[0] = tri[0], tri[1]
 	}
-	if poly[2].Y < poly[1].Y {
-		poly[2], poly[1] = poly[1], poly[2]
+	if tri[2].Y < tri[1].Y {
+		tri[2], tri[1] = tri[1], tri[2]
 	}
-	if poly[1].Y < poly[0].Y {
-		poly[1], poly[0] = poly[0], poly[1]
-	}
-
-	// calculate delta values
-	l, r := poly[0], poly[0]
-	dx1, dx2, dx3 := 0.0, 0.0, 0.0
-	if poly[1].Y-poly[0].Y > 0 {
-		dx1 = (poly[1].X - poly[0].X) / (poly[1].Y - poly[0].Y)
-	}
-	if poly[2].Y-poly[0].Y > 0 {
-		dx2 = (poly[2].X - poly[0].X) / (poly[2].Y - poly[0].Y)
-	}
-	if poly[2].Y-poly[1].Y > 0 {
-		dx3 = (poly[2].X - poly[1].X) / (poly[2].Y - poly[1].Y)
+	if tri[1].Y < tri[0].Y {
+		tri[1], tri[0] = tri[0], tri[1]
 	}
 
-	// main draw routine
-	if dx1 > dx2 {
-		// draw bottom half
-		for ; l.Y <= poly[1].Y; l.Y, l.X, r.Y, r.X = l.Y+1, l.X+dx2, r.Y+1, r.X+dx1 {
-			for i := l.X; i < r.X; i++ {
-				c.SmartSet(int(i), int(l.Y), color)
-			}
+	// determine linear equations for bottom half
+	botFn1 := func(y float64) float64 {
+		if tri[1].X == tri[0].X || tri[1].Y == tri[0].Y {
+			return tri[1].X
 		}
-		// draw top half
-		r = poly[1]
-		for ; l.Y <= poly[2].Y; l.Y, l.X, r.Y, r.X = l.Y+1, l.X+dx2, r.Y+1, r.X+dx3 {
-			for i := l.X; i < r.X; i++ {
-				c.SmartSet(int(i), int(l.Y), color)
-			}
+		m := (tri[1].Y - tri[0].Y) / (tri[1].X - tri[0].X)
+		return (y - tri[0].Y + m*tri[0].X) / m
+	}
+	botFn2 := func(y float64) float64 {
+		if tri[2].X == tri[0].X || tri[2].Y == tri[0].Y {
+			return tri[2].X
 		}
-	} else {
-		for ; l.Y <= poly[1].Y; l.Y, l.X, r.Y, r.X = l.Y+1, l.X+dx1, r.Y+1, r.X+dx2 {
-			for i := l.X; i < r.X; i++ {
-				c.SmartSet(int(i), int(l.Y), color)
-			}
+		m := (tri[2].Y - tri[0].Y) / (tri[2].X - tri[0].X)
+		return (y - tri[0].Y + m*tri[0].X) / m
+	}
+	// sort by X value
+	if botFn2(tri[0].Y+1) < botFn1(tri[0].Y+1) {
+		botFn1, botFn2 = botFn2, botFn1
+	}
+
+	// draw bottom half
+	for i := tri[0].Y; i <= tri[1].Y; i++ {
+		l, r := botFn1(i), botFn2(i)
+		// draw scanline
+		for j := l; j <= r; j++ {
+			c.SmartSet(int(j), int(i), color)
 		}
-		l = poly[1]
-		for ; l.Y <= poly[2].Y; l.Y, l.X, r.Y, r.X = l.Y+1, l.X+dx3, r.Y+1, r.X+dx2 {
-			for i := l.X; i < r.X; i++ {
-				c.SmartSet(int(i), int(l.Y), color)
-			}
+	}
+	// determine linear equations for top half
+	topFn1 := func(y float64) float64 {
+		if tri[2].X == tri[0].X || tri[2].Y == tri[0].Y {
+			return tri[2].X
+		}
+		m := (tri[2].Y - tri[0].Y) / (tri[2].X - tri[0].X)
+		return (y - tri[0].Y + m*tri[0].X) / m
+	}
+	topFn2 := func(y float64) float64 {
+		if tri[2].X == tri[1].X || tri[2].Y == tri[1].Y {
+			return tri[2].X
+		}
+		m := (tri[2].Y - tri[1].Y) / (tri[2].X - tri[1].X)
+		return (y - tri[2].Y + m*tri[2].X) / m
+	}
+	// sort by X value
+	if topFn2(tri[1].Y+1) < topFn1(tri[1].Y+1) {
+		topFn1, topFn2 = topFn2, topFn1
+	}
+
+	// draw top half
+	for i := tri[1].Y; i <= tri[2].Y; i++ {
+		l, r := topFn1(i), topFn2(i)
+		// draw scanline
+		for j := l; j <= r; j++ {
+			c.SmartSet(int(j), int(i), color)
 		}
 	}
 
 	// draw outline
-	c.DrawLine(Line{poly[0], poly[1]})
-	c.DrawLine(Line{poly[1], poly[2]})
-	c.DrawLine(Line{poly[2], poly[0]})
+	c.DrawLine(Line{tri[0], tri[1]})
+	c.DrawLine(Line{tri[1], tri[2]})
+	c.DrawLine(Line{tri[2], tri[0]})
 }
 
 func main() {
@@ -83,41 +102,41 @@ func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	// canvas properties
 	width, height := 700, 700
-	// figure
-	figure := [12]Polygon{
-		// front
-		Polygon{Vector{-100, 100, 100}, Vector{100, 100, 100}, Vector{-100, -100, 100}},
-		Polygon{Vector{100, -100, 100}, Vector{100, 100, 100}, Vector{-100, -100, 100}},
-		// back
-		Polygon{Vector{-100, 100, -100}, Vector{100, 100, -100}, Vector{-100, -100, -100}},
-		Polygon{Vector{100, -100, -100}, Vector{100, 100, -100}, Vector{-100, -100, -100}},
-		// top
-		Polygon{Vector{-100, 100, 100}, Vector{100, 100, 100}, Vector{-100, 100, -100}},
-		Polygon{Vector{100, 100, -100}, Vector{100, 100, 100}, Vector{-100, 100, -100}},
-		// bottom
-		Polygon{Vector{-100, -100, 100}, Vector{100, -100, 100}, Vector{-100, -100, -100}},
-		Polygon{Vector{100, -100, -100}, Vector{100, -100, 100}, Vector{-100, -100, -100}},
-		// left
-		Polygon{Vector{100, -100, 100}, Vector{100, 100, 100}, Vector{100, -100, -100}},
-		Polygon{Vector{100, 100, -100}, Vector{100, 100, 100}, Vector{100, -100, -100}},
-		// right
-		Polygon{Vector{-100, -100, 100}, Vector{-100, 100, 100}, Vector{-100, -100, -100}},
-		Polygon{Vector{-100, 100, -100}, Vector{-100, 100, 100}, Vector{-100, -100, -100}}}
+	// figure (vertices must be defined in clockwise order)
+	figure := [12]Triangle{
+		// front (Z = 100)
+		Triangle{Vector{-100, 100, 100}, Vector{100, 100, 100}, Vector{-100, -100, 100}},
+		Triangle{Vector{-100, -100, 100}, Vector{100, 100, 100}, Vector{100, -100, 100}},
+		// top (Y = 100)
+		Triangle{Vector{-100, 100, -100}, Vector{100, 100, -100}, Vector{-100, 100, 100}},
+		Triangle{Vector{-100, 100, 100}, Vector{100, 100, -100}, Vector{100, 100, 100}},
+		// right (X = 100)
+		Triangle{Vector{100, 100, 100}, Vector{100, 100, -100}, Vector{100, -100, 100}},
+		Triangle{Vector{100, -100, 100}, Vector{100, 100, -100}, Vector{100, -100, -100}},
+		// back (Z = -100)
+		Triangle{Vector{100, 100, -100}, Vector{-100, 100, -100}, Vector{100, -100, -100}},
+		Triangle{Vector{100, -100, -100}, Vector{-100, 100, -100}, Vector{-100, -100, -100}},
+		// bottom (Y = -100)
+		Triangle{Vector{-100, -100, 100}, Vector{100, -100, 100}, Vector{-100, -100, -100}},
+		Triangle{Vector{-100, -100, -100}, Vector{100, -100, 100}, Vector{100, -100, -100}},
+		// left (X = -100)
+		Triangle{Vector{-100, 100, -100}, Vector{-100, 100, 100}, Vector{-100, -100, -100}},
+		Triangle{Vector{-100, -100, -100}, Vector{-100, 100, 100}, Vector{-100, -100, 100}}}
 	// draw loop
 	numSteps := 50
-	frameChan := make(chan int, numSteps)
 	for i := 0; i < numSteps; i++ {
-		frameChan <- i
 		// draw each frame in a separate goroutine
-		go func() {
-			num := <-frameChan
+		go func(num int) {
 			canvas := NewCanvas(width, height)
 			tMatrix := RotationMatrix(float64(num*360/numSteps), Vector{1, 1, 1})
 			for j := range figure {
-				canvas.DrawPoly(figure[j].Transform(tMatrix).Projection(), color.RGBA{uint8(j * 20), 0, uint8(255 - j*20), 255})
+				tTri := figure[j].Transform(tMatrix)
+				if tTri.IsVisible() {
+					canvas.DrawTri(tTri, color.RGBA{uint8(j * 20), 0, uint8(255 - j*20), 255})
+				}
 			}
 			canvas.SaveToFile(num)
-		}()
+		}(i)
 	}
 	// animation and clean-up
 	log.Print("finished computation")
